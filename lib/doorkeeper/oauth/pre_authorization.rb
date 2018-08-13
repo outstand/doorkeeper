e14030c2366e7b9a0d0a467b4fc84a9409f15fa5
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-08-13 16:45:03 -0700

    Backport fixes from #1060

--- lib/doorkeeper/oauth/pre_authorization.rb
+++ lib/doorkeeper/oauth/pre_authorization.rb
@@ -55,11 +55,13 @@ module Doorkeeper
         )
       end
 
-      # TODO: test uri should be matched against the client's one
       def validate_redirect_uri
-        return false unless redirect_uri.present?
-        Helpers::URIChecker.native_uri?(redirect_uri) ||
-          Helpers::URIChecker.valid_for_authorization?(redirect_uri, client.redirect_uri)
+        return false if redirect_uri.blank?
+
+        Helpers::URIChecker.valid_for_authorization?(
+          redirect_uri,
+          client.redirect_uri
+        )
       end
     end
   end
